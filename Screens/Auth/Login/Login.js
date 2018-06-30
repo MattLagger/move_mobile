a4be6ff689d33e25354ab2c275dcd63c746fc260
Wendreo Matheus <wendreo.pvh@gmail.com>
2018-06-30 17:30:08 -0500

    Stylish Login and Register

--- Screens/Auth/Login/Login.js
+++ Screens/Auth/Login/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {ImageBackground, Image} from 'react-native'
 import {Button, Text, Item, Form, Label, Input, Container, Content} from 'native-base'
+import LinearGradient from 'react-native-linear-gradient'
 
 import { LoginAction } from '../../../Config/Auth';
 
@@ -24,22 +26,41 @@ class Login extends Component {
 
 	render() {
 		return (
-			<Container style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-				<Content style={{width: 300}}>
-					<Form>
-						<Item floatingLabel>
-						<Label>Email</Label>
-						<Input />
-						</Item>
-						<Item floatingLabel last>
-						<Label>Password</Label>
-						<Input />
-						</Item>
-					</Form>
-					<Button block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Home')}><Text>Login</Text></Button>
-					<Button block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Signup')}><Text>Cadastro</Text></Button>
-					<Text style={{fontSize: 20, color: '#1F4788', textAlign:'center', marginTop: 15, textDecorationLine: 'underline'}}>Recuperar Senha</Text>
-				</Content>
+			<Container>
+				<ImageBackground 
+					source={require('../../../Assets/bg.jpg')}
+					blurRadius={3}
+					style={{
+						width: '100%', 
+						height: '100%',
+						flex: 1,
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+					>
+					<LinearGradient colors={['rgba(15, 33, 64, 0.8)', 'rgba(52, 120, 229, 0.6)']}>
+						<Content style={{width: '100%', padding: '15%'}}>
+							<Image style={{flex: 1, width: 300}} resizeMode='contain' source={require('../../../Assets/logo.png')}/>
+							<Form>
+								<Item floatingLabel last>
+								<Label style={{color: '#fff'}}>Email</Label>
+								<Input 
+									style={{color: "white"}}
+								/>
+								</Item>
+								<Item floatingLabel last>
+								<Label style={{color: '#fff'}}>Senha</Label>
+								<Input 
+									style={{color: "white"}}
+								/>
+								</Item>
+							</Form>
+							<Button light bordered block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Home')}><Text>Login</Text></Button>
+							<Button light bordered block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Signup')}><Text>Cadastro</Text></Button>
+							<Text style={{fontSize: 20, color: '#fff', textAlign:'center', marginTop: 15, textDecorationLine: 'underline'}}>Recuperar Senha</Text>
+						</Content>
+					</LinearGradient>
+				</ImageBackground>
 			</Container>
 		);
 }
--- Screens/Auth/Signup/Signup.js
+++ Screens/Auth/Signup/Signup.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
 import {Button, Text, Item, Form, Label, Input, Container, Content, H1} from 'native-base'
+import LinearGradient from 'react-native-linear-gradient'
 
 export default class Signup extends Component {
 	render() {
 		return (
-			<Container style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-				<Content style={{width: 300}}>
-				<H1 style={{textAlign: 'center', marginTop: 30}}>Cadastro</H1>
-					<Form>
-						<Item floatingLabel>
-							<Label>Email</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel>
-							<Label>Nome</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel>
-							<Label>Telefone</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel>
-							<Label>Cidade</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel>
-							<Label>Estado</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel>
-							<Label>Password</Label>
-							<Input />
-						</Item>
-						<Item floatingLabel last>
-							<Label>ConfirmPassword</Label>
-							<Input />
-						</Item>
-					</Form>
-					<Button block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Signup')}><Text>Cadastro</Text></Button>
-					<Text onPress={() => this.props.navigation.navigate('Login')} style={{fontSize: 20, color: '#1F4788', textAlign:'center', marginTop: 15, textDecorationLine: 'underline'}}>Já Cadastrado ?</Text>
-				</Content>
+			<Container>
+				<LinearGradient style={{flex: 1, alignItems: 'center', justifyContent:'center'}} colors={['rgba(15, 33, 64, 1)', 'rgba(29, 67, 127, 1)']} >
+					<Content style={{width: 300}}>
+					<H1 style={{textAlign: 'center', marginTop: 30, color: '#fff'}}>Cadastro</H1>
+						<Form>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Email</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Nome</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Telefone</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Cidade</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Estado</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Senha</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+							<Item floatingLabel>
+								<Label style={{color: '#fff'}}>Confirmar Senha</Label>
+								<Input style={{color: '#fff'}} />
+							</Item>
+						</Form>
+						<Button light bordered block style={{marginTop: 20}} onPress={() => this.props.navigation.navigate('Signup')}><Text>Cadastrar</Text></Button>
+						<Text onPress={() => this.props.navigation.navigate('Login')} style={{fontSize: 20, color: '#fff', textAlign:'center', marginTop: 15, textDecorationLine: 'underline'}}>Já Cadastrado ?</Text>
+					</Content>
+				</LinearGradient>
 			</Container>
 		);
 }
