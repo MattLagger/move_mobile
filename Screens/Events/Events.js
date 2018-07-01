4a46d155c9376aa91b2bc4e5b763cd36bab42d4b
Wendreo Matheus <wendreo.pvh@gmail.com>
2018-06-30 23:40:25 -0500

    Display Event

--- Screens/Events/Events.js
+++ Screens/Events/Events.js
@@ -1,6 +1,67 @@
-import React from 'react'
-import {Button} from 'react-native'
+import React, {Component} from 'react';
+import { Image, Modal, ScrollView, TouchableOpacity } from 'react-native';
+import {Card, CardItem, Thumbnail, Text, Button, Left, Body, Right } from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-export default (props) => (
-    <Button title='Login' onPress={() => props.navigation.navigate('Login')} />
-)
+import Event from './Event'
+
+export default class extends Component {
+    state = {
+        isVisible: false
+    }
+
+    setVisibilty = () => {
+        this.setState({isVisible: !this.state.isVisible})
+    }
+
+    render(){
+        return (
+            <ScrollView>
+                <Modal visible={this.state.isVisible} onRequestClose={() => this.setVisibilty()}>
+                    <Event displayEvent={this.setVisibilty} />
+                </Modal>
+                <TouchableOpacity onPress={() => this.setVisibilty()}>
+                <Card>
+                    <CardItem>
+                        <Left>
+                        <Thumbnail source={{uri: 'https://twistedsifter.files.wordpress.com/2012/09/trippy-profile-pic-portrait-head-on-and-from-side-angle.jpg?w=800&h=700&zoom=2'}} />
+                        <Body>
+                            <Text>Wendreo Teixeira</Text>
+                        </Body>
+                        </Left>
+                        <Right>
+                            <Button transparent>
+                                <Text style={{fontSize: 20, color:'#1f3a93'}}>12</Text>
+                                <Icon name="star" size={25} color='#1f3a93' />
+                            </Button>
+                        </Right>
+                    </CardItem>
+                    <CardItem cardBody>
+                        <Image source={{uri: 'https://cdn.internationalinsurance.com/wp-content/uploads/2014/03/extreme-sport-travel-insurance.jpg'}} style={{height: 200, width: null, flex: 1}}/>
+                    </CardItem>
+                    <CardItem>
+                        <Body>
+                            <Text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                                ultrices, tortor sed interdum volutpat, orci leo accumsan est, 
+                                a cras amet...
+                            </Text>
+                        </Body>   
+                    </CardItem>
+                    <CardItem>
+                        <Left>
+                            <Icon name="place"size={20} color='#a1a1a1a1' />
+                            <Text style={{color: '#a1a1a1', fontSize: 15}}>Universidade Federal do Acre</Text>
+                        </Left>
+                        <Body />
+                        <Right style={{flex: 1, alignItems: 'center', flexDirection: 'row-reverse'}}>
+                            <Icon name="alarm"size={20} color='#a1a1a1' />  
+                            <Text style={{color: '#a1a1a1', fontSize: 15}}>01/03/1995</Text>
+                        </Right>
+                    </CardItem>
+                </Card>
+                </TouchableOpacity>
+            </ScrollView>
+        )    
+    }
+}
